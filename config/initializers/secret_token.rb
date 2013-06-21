87c174fc558f1587c6d4b80e312fb7c7c6447189
Christopher Fernández <fernandez.chl@gmail.com>
2013-06-21 17:42:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chivo::Application.config.secret_token = '68c579534573d2b204688bcc0f4957bdaf3672daa71e3a73580baa7063d9094af5c8081ef1c4ff781822e61b1c3bf0162320e9efcb70f80dfd3708c1cc79d415'
